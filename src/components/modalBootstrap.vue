8da5e0af58e93cd334ee47f7fa630610bbeb3c74
Marlon Velasquez <marlonjvelasquez@gmail.com>
2021-05-13 13:04:46 +0200

    modalBootstrap.vue Created component with a modal.
    Deleted HelloWord component by default

---
+++ src/components/modalBootstrap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#exampleModal"
+    >
+      Abrir Modal
+    </button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="headerSlot" />
+          </div>
+          <div class="modal-body d-flex flex-column align-items-center">
+            <slot name="bodySlot" />
+          </div>
+          <div class="modal-footer">
+            <slot name="footerSlot" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bootstrapModal",
+};
+</script>
+
+<style scoped></style>
